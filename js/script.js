aae40991684d208588ff357be24e0406cadfcfe0
Pedro Machado <pedromchd@outlook.com.br>
2022-10-13 21:03:20 -0300

    commit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 import { enem2017 } from './enem2017.js';
 
 $(document).ready(function () {
-
+  $('.questao-content').html(enem2017.questao(136).enunciados.map((enunciado) => `<p>${enunciado}</p>`));
 });
