f4a8fe0449a170b77ce0c7d701da7a5b623e192c
Pedro Machado <pedromchd@outlook.com.br>
2022-12-04 12:22:43 -0300

    buscar.js

--- js/buscar.js
+++ js/buscar.js
@@ -1,7 +1,13 @@
-onmessage = async (msg) => {
+import enem2017 from './json/enem2017.json' assert { type: 'json' };
+
+onmessage = (msg) => {
   const ano = msg.data.ano;
   const area = msg.data.area;
   const materia = msg.data.materia;
   const conteudo = msg.data.conteudo;
-  postMessage(ano);
+  postMessage(enem2017['136']._gabarito);
 };
+
+function buscarAno(ano) {
+
+}
